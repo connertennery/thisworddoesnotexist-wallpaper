0b75a69fba382db5b50e7432c8344c9be9e69957
Conner Tennery <contact@connertennery.dev>
2020-05-16 01:18:29 -0500

    Downloads new word, makes image, sets background

---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const webshot = require('webshot');
+const sharp = require('sharp');
+const wallpaper = require('wallpaper');
+
+const desiredOutputResolution = {
+	width: 2560,
+	height: 1440,
+};
+
+const options = {
+	windowSize: { width: 4000, height: 4000 },
+	quality: 100,
+	customCSS: `li:nth-child(2) { 
+		display: none;
+	}`,
+	captureSelector: '.inner',
+};
+
+webshot('https://www.thisworddoesnotexist.com/', 'fakeword.png', options, function (err) {
+	console.error(err);
+});
+
+const watcher = fs.watch('./', (eventType, filename) => {
+	if (filename) {
+		console.log(eventType, filename);
+		if (eventType !== 'change' || filename !== 'fakeword.png') return;
+
+		processImage();
+	}
+});
+
+const debounce = (func, time) => {
+	let debounceTimer;
+	return function () {
+		const context = this;
+		const args = arguments;
+		clearTimeout(debounceTimer);
+		debounceTimer = setTimeout(() => func.apply(context, args), time);
+	};
+};
+
+const processImage = debounce(function () {
+	const image = sharp('fakeword.png', { failOnError: false });
+
+	image
+		.metadata()
+		.then((metadata) => {
+			const verticalExtension = Math.floor(desiredOutputResolution.height / 2 - metadata.height / 2);
+			const horizontalExtension = Math.floor(desiredOutputResolution.width / 2 - metadata.width / 2);
+			const extendOptions = {
+				top: verticalExtension,
+				bottom: verticalExtension,
+				left: horizontalExtension,
+				right: horizontalExtension,
+				background: '#08082d',
+			};
+			console.log(extendOptions);
+			return image.extend(extendOptions);
+		})
+		.then((image) => {
+			image
+				.toFile('fakeword-final.png', { quality: 100 })
+				.then((info) => {
+					console.log(info);
+					(async () => {
+						await wallpaper.set('fakeword-final.png');
+						watcher.close();
+					})();
+				})
+				.catch((err) => console.error(err));
+		});
+}, 100);
